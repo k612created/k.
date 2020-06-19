0dae4d42493687bb3938e5922f7275b9f8f9e780
Sam Huang <63796156+k612created@users.noreply.github.com>
2020-06-19 22:24:12 +0800

    Update kInt.hpp

--- kInt.hpp
+++ kInt.hpp
@@ -1,8 +1,16 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
 class kInt {
+	private:
+		string content = "";
+	public:
+		kInt(int x) : content (stringstream(x).str()) {}
+		kInt(string x) : content(x) {}
+		kInt(const char* x) : content(string(x)) {}
 	
 };
